77e9b8115e483b348afe68262f16d8b244fccd0a
Adam57 <qiwangwq@amazon.com>
2022-02-13 18:00:50 -0800

    multi thread testing

--- examples/test-rank-mq2008-tree.rs
+++ examples/test-rank-mq2008-tree.rs
@@ -1,54 +1,70 @@
 extern crate gbdt;
+use std::convert::TryFrom;
+use std::thread;
+use std::time::Duration;
 
-use gbdt::decision_tree::{PredVec, ValueType};
+use gbdt::decision_tree::PredVec;
 use gbdt::gradient_boost::GBDT;
 use gbdt::input;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
 
 fn main() {
-    // Call this command to convert xgboost model:
-    // python examples/convert_xgboost.py xgb-data/xgb_rank_pairwise/xgb.model "rank:pairwise" xgb-data/xgb_rank_pairwise/gbdt.model
-    // load model
-    let gbdt = GBDT::from_xgoost_dump("examples/my_model.json", "rank:pairwise")
-        .expect("failed to load model");
+    // // Call this command to convert xgboost model:
+    // // python examples/convert_xgboost.py xgb-data/xgb_rank_pairwise/xgb.model "rank:pairwise" xgb-data/xgb_rank_pairwise/gbdt.model
+    // // load model
+    // let gbdt = GBDT::from_xgoost_dump("examples/my_model.json", "rank:pairwise")
+    //     .expect("failed to load model");
 
-    // load test data
-    let test_file = "xgb-data/xgb_rank_pairwise/mq2008.test";
-    let mut input_format = input::InputFormat::txt_format();
-    input_format.set_feature_size(47);
-    input_format.set_delimeter(' ');
-    let test_data = input::load(test_file, input_format).expect("failed to load test data");
+    // // load test data
+    // let test_file = "xgb-data/xgb_rank_pairwise/mq2008.test";
+    // let mut input_format = input::InputFormat::txt_format();
+    // input_format.set_feature_size(47);
+    // input_format.set_delimeter(' ');
+    // let ori_test_data = input::load(test_file, input_format).expect("failed to load test data");
 
-    // inference
-    println!("start prediction");
-    let predicted: PredVec = gbdt.predict(&test_data);
-    assert_eq!(predicted.len(), test_data.len());
+    let mut handles = Vec::new();
 
-    // compare to xgboost prediction results
-    let predict_result = "xgb-data/xgb_rank_pairwise/pred.csv";
+    let mut total = Duration::new(0, 0);
 
-    let mut xgb_results = Vec::new();
-    let file = File::open(predict_result).expect("failed to load pred.csv");
-    let reader = BufReader::new(file);
-    for line in reader.lines() {
-        let text = line.expect("failed to read data from pred.csv");
-        let value: ValueType = text.parse().expect("failed to parse data from pred.csv");
-        xgb_results.push(value);
+    let num_query = 30;
+
+    for _x in 0..num_query {
+        let handle = thread::spawn(move || {
+            // load model
+            let gbdt = GBDT::from_xgoost_dump("examples/my_model.json", "rank:pairwise")
+                .expect("failed to load model");
+
+            // load test data
+            let test_file = "xgb-data/xgb_rank_pairwise/mq2008.test";
+            let mut input_format = input::InputFormat::txt_format();
+            input_format.set_feature_size(47);
+            input_format.set_delimeter(' ');
+            let test_data = input::load(test_file, input_format).expect("failed to load test data");
+
+            // inference
+            println!("start prediction");
+            use std::time::Instant;
+            let now = Instant::now();
+            let predicted: PredVec = gbdt.predict(&test_data);
+            let elapsed = now.elapsed();
+            println!("Elapsed: {:.2?}", elapsed);
+            let data_size = u32::try_from(test_data.len());
+
+            println!("Per Query: {:.2?}", elapsed / data_size.unwrap());
+
+            assert_eq!(predicted.len(), test_data.len());
+            println!("{}", test_data.len());
+            println!("prediction Done");
+            elapsed
+        });
+        handles.push(handle);
     }
 
-    let mut max_diff: ValueType = -1.0;
-    for (value1, value2) in predicted.iter().zip(xgb_results.iter()) {
-        println!("{} {}", value1, value2);
-        let diff = (value1 - value2).abs();
-        if diff > max_diff {
-            max_diff = diff;
-        }
+    for handle in handles {
+        let per_q = handle.join().unwrap();
+        total += per_q;
     }
 
-    println!(
-        "Compared to results from xgboost, max error is: {:.10}",
-        max_diff
-    );
-    assert!(max_diff < 0.01);
+    println!("number of query: {}", num_query);
+    println!("average per query: {:.2?}", total / num_query);
+    println!("average per doc: {:.2?}", total / num_query / 200);
 }
